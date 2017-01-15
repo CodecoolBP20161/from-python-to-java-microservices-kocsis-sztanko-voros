ad61984383582839b3d0444d59decd9db17689a6
dsztanko <szt.dori@gmail.com>
2017-01-15 16:40:49 +0100

    daomem tested

--- src/test/java/you_might_also_like_service/dao/inmemoryimplementation/UserDaoMemTest.java
+++ src/test/java/you_might_also_like_service/dao/inmemoryimplementation/UserDaoMemTest.java
@@ -9,7 +9,7 @@ import you_might_also_like_service.model.User;
 import java.util.Arrays;
 import java.util.HashSet;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 public class UserDaoMemTest {
     UserDao userDao;
@@ -29,7 +29,7 @@ public class UserDaoMemTest {
 
     @After
     public void tearDown() throws Exception {
-        userDao = null;
+        userDao.getAll().clear();
         accessToken = null;
         userID = null;
         cartItemID = null;
@@ -67,13 +67,38 @@ public class UserDaoMemTest {
     }
 
     @Test
-    public void containsOneOfTheSpecItemsAtLeast() throws Exception {
+    public void testContainsOneOfTheSpecItemsAtLeast_missingAccessToken() throws Exception {
+        userDao.save(accessToken, "1", "1");
+        userDao.save(accessToken, "1", "2");
+        userDao.save(accessToken, "2", "1");
+        userDao.save(accessToken, "2", "2");
+        userDao.save(accessToken, "3", "9");
+        userDao.save(accessToken, "3", "10");
+        userDao.save(accessToken, "4", "5");
+        assertEquals(2, userDao.containsOneOfTheSpecItemsAtLeast(accessToken, "1").size());
+    }
 
+        @Test
+    public void containsOneOfTheSpecItemsAtLeastFails_emptyAccessToken() throws Exception {
+        assertEquals(0, userDao.containsOneOfTheSpecItemsAtLeast("", "1").size());
+    }
+
+    @Test
+    public void containsOneOfTheSpecItemsAtLeastFails_emptyUserId() throws Exception {
+        assertEquals(0, userDao.containsOneOfTheSpecItemsAtLeast(accessToken, "").size());
     }
 
     @Test
     public void selectUniqueItems() throws Exception {
-
+        cart = new HashSet<>(Arrays.asList("1", "2"));
+        testUser = new User(accessToken, "1", cart);
+        userDao.save(accessToken, "1", "1");
+        userDao.save(accessToken, "1", "2");
+        userDao.save(accessToken, "2", "1");
+        userDao.save(accessToken, "2", "3");
+        userDao.save(accessToken, "2", "9");
+        userDao.save(accessToken, "3", "10");
+        userDao.save(accessToken, "4", "5");
+        assertEquals(2, userDao.selectUniqueItems(accessToken, testUser).size());
     }
-
 }
